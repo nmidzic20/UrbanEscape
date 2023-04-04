75d49f783fa85e388ad19cb5f1df9d0d88ff79df
Noa Midzic <nmidzic@yahoo.com>
2023-04-04 22:22:20 +0200

    Adjusted custom theme, added top app bar and navigation drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:urban_escape/theme/theme_constants.dart';
 import 'package:urban_escape/theme/theme_manager.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   void dispose() {
     _themeManager.removeListener(themeListener);
@@ -31,9 +31,7 @@ class _MyAppState extends State<MyApp> {
 
   void themeListener() {
     if (mounted) {
-      setState(() {
-
-      });
+      setState(() {});
     }
   }
 
@@ -42,9 +40,9 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Urban Escape',
       debugShowCheckedModeBanner: false,
-      //theme: ThemeData(primarySwatch: Color(0x262235)),//lightTheme,
-      //darkTheme: darkTheme,
-      //themeMode: _themeManager.themeMode,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: _themeManager.themeMode,
       home: const HomePage(),
     );
   }
@@ -57,11 +55,39 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Urban Escape"),
+        // Necessary to put leading: Builder(...) { return IconButton(...)} instead of leading: IconButton(...)
+        // in order to be able to call context for Scaffold.of (to open the drawer on clicking the icon)
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () => Scaffold.of(context).openDrawer(),
+          );
+        }),
+        title: Text('Home'),
+        actions: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+          ),
+          Icon(Icons.account_circle),
+        ],
       ),
-      body: const Center(
-        child: Text("Proba"),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            Switch(
+                value: false,
+                onChanged: (newValue) {
+                  //Navigator.pop(context); too fast, need to see the switch changed
+                }),
+          ],
+        ),
       ),
+      body: Container(),
     );
   }
 }
