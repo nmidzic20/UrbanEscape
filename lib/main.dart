d68bbc46fe1170efc06a94ced943ed4a35625816
Noa Midzic <nmidzic@yahoo.com>
2023-04-04 18:33:34 +0200

    Resized splash 800x1000, added custom theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:urban_escape/theme/theme_constants.dart';
+import 'package:urban_escape/theme/theme_manager.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+ThemeManager _themeManager = ThemeManager();
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void dispose() {
+    _themeManager.removeListener(themeListener);
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    _themeManager.addListener(themeListener);
+    super.initState();
+  }
+
+  void themeListener() {
+    if (mounted) {
+      setState(() {
+
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Urban Escape',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      //theme: ThemeData(primarySwatch: Color(0x262235)),//lightTheme,
+      //darkTheme: darkTheme,
+      //themeMode: _themeManager.themeMode,
       home: const HomePage(),
     );
   }
---
+++ lib/theme/theme_constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const COLOR_PRIMARY = Color(0x262235); //deep purple
+
+
+ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: COLOR_PRIMARY
+);
+
+ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark
+);
