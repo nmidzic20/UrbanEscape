358281f695107552e7a9b39b79962dda79f75bbb
Tin Pojatina <tin.p99@gmail.com>
2023-05-06 09:14:24 +0200

    Hint-Answer
    Init

---
+++ lib/Widgets/AnswerCheck.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AnswerCheck extends StatelessWidget {
+  TextEditingController _textFieldController = TextEditingController();
+  final String _premadeString = "I WANNA DIE!!!";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Answer Check'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text('Question'),
+                  content: TextField(
+                    controller: _textFieldController,
+                    decoration: InputDecoration(hintText: 'Answer'),
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      child: Text('CANCEL'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    TextButton(
+                      child: const Text('OK'),
+                      onPressed: () {
+                        String userInput = _textFieldController.text;
+                        String result = '';
+                        if (userInput == _premadeString) {
+                          print("Correct answer: $userInput");
+                          result = 'Correct answer!';
+                        } else {
+                          print("Wrong answer, the correct answer is: $_premadeString");
+                          result = 'Wrong answer.';
+                        }
+
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text('Answer Check'),
+                              content: Text(result),
+                              actions: [
+                                TextButton(
+                                  child: const Text('OK'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          child: const Text('User Input'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Widgets/ScoreCount.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class ScoreScreen extends StatefulWidget {
+  int initialScore;
+  int maxScore;
+  Duration countdownDuration;
+
+  ScoreScreen({
+    this.initialScore = 100,
+    this.maxScore = 0,
+    this.countdownDuration = const Duration(seconds: 30)
+  });
+
+
+  @override
+  _ScoreScreenState createState() => _ScoreScreenState();
+}
+
+class _ScoreScreenState extends State<ScoreScreen> {
+  late int _score;
+  late int _timeLeft;
+  late Timer _countdownTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    _score = widget.initialScore;
+    _timeLeft = _calculateTimeLeft();
+    _startCountdown();
+  }
+
+  @override
+  void dispose() {
+    _stopCountdown();
+    super.dispose();
+  }
+
+  void _startCountdown() {
+    _countdownTimer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_score > widget.maxScore) {
+          _score--;
+          _timeLeft = _calculateTimeLeft();
+        } else {
+          _stopCountdown();
+        }
+      });
+    });
+  }
+
+  void _stopCountdown() {
+      _countdownTimer.cancel();
+  }
+
+  int _calculateTimeLeft() {
+    int timeLeft = ((widget.countdownDuration.inSeconds * (widget.maxScore - _score)) / (widget.initialScore - widget.maxScore)).ceil();
+    return timeLeft;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Score: $_score',
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Time left: $_timeLeft seconds',
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ],
+          ),
+          SizedBox(height: 16.0),
+          TextButton(
+            child: Text('Stop'),
+            onPressed: _stopCountdown,
+          ),
+        ],
+      ),
+    );
+  }
+}
